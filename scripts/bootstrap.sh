b03d16edc9afc77e05638bc4cd86ca29f5212f05
Lannesteer <degterev907@gmail.com>
2026-02-15 01:29:27 +0300

    crated dev artifacts workflow and artifact.overrides.dev.yml

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -75,7 +75,7 @@ bootstrap_manager() {
 
 
   # XRAY CONFIG VERSION: bump this when you WANT to create a new Swarm config
-  XRAY_CONFIG_VERSION="${XRAY_CONFIG_VERSION:-V3_0}"
+  XRAY_CONFIG_VERSION="${XRAY_CONFIG_VERSION:-V3_1}"
 
   # Fallback assets
   VPN_FALLBACK_INDEX_VERSION="${VPN_FALLBACK_INDEX_VERSION:-V1_1}"
