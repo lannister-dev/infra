9372c500c753ab98e2b971d2e53400dba9f6640b
Lannesteer <degterev907@gmail.com>
2026-02-23 03:30:47 +0300

    fix

--- terraform/nodes/providers.tf
+++ terraform/nodes/providers.tf
@@ -24,24 +24,16 @@ terraform {
 provider "openstack" {
   alias = "hostvds"
 
-  auth_url     = trimspace(var.hostvds_os_auth_url) != "" ? var.hostvds_os_auth_url : null
-  user_name    = trimspace(var.hostvds_os_username) != "" ? var.hostvds_os_username : null
-  password     = trimspace(var.hostvds_os_password) != "" ? var.hostvds_os_password : null
-  tenant_name  = trimspace(var.hostvds_os_project_name) != "" ? var.hostvds_os_project_name : null
+  auth_url    = trimspace(var.hostvds_os_auth_url) != "" ? var.hostvds_os_auth_url : null
+  user_name   = trimspace(var.hostvds_os_username) != "" ? var.hostvds_os_username : null
+  password    = trimspace(var.hostvds_os_password) != "" ? var.hostvds_os_password : null
+  tenant_name = trimspace(var.hostvds_os_project_name) != "" ? var.hostvds_os_project_name : null
 
-  user_domain_id = trimspace(var.hostvds_os_user_domain_id) != "" ? var.hostvds_os_user_domain_id : null
-  user_domain_name = (
-    trimspace(var.hostvds_os_user_domain_id) == "" && trimspace(var.hostvds_os_user_domain_name) != ""
-    ? var.hostvds_os_user_domain_name
-    : null
-  )
+  user_domain_id   = trimspace(var.hostvds_os_user_domain_id) != "" ? var.hostvds_os_user_domain_id : null
+  user_domain_name = trimspace(var.hostvds_os_user_domain_id) == "" && trimspace(var.hostvds_os_user_domain_name) != "" ? var.hostvds_os_user_domain_name : null
 
-  project_domain_id = trimspace(var.hostvds_os_project_domain_id) != "" ? var.hostvds_os_project_domain_id : null
-  project_domain_name = (
-    trimspace(var.hostvds_os_project_domain_id) == "" && trimspace(var.hostvds_os_project_domain_name) != ""
-    ? var.hostvds_os_project_domain_name
-    : null
-  )
+  project_domain_id   = trimspace(var.hostvds_os_project_domain_id) != "" ? var.hostvds_os_project_domain_id : null
+  project_domain_name = trimspace(var.hostvds_os_project_domain_id) == "" && trimspace(var.hostvds_os_project_domain_name) != "" ? var.hostvds_os_project_domain_name : null
 
   region        = trimspace(var.hostvds_os_region_name) != "" ? var.hostvds_os_region_name : null
   endpoint_type = trimspace(var.hostvds_os_interface) != "" ? var.hostvds_os_interface : null
