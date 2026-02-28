d667c7b68046b9e1d2c68f31730a881cc4799adb
Lannesteer <degterev907@gmail.com>
2026-02-28 23:28:38 +0300

    created reality config vless-reality tcp and reality-dokodemo. Edited terraform files, port published 443 added to docker compose

--- terraform/foundation/variables.tf
+++ terraform/foundation/variables.tf
@@ -13,6 +13,30 @@ variable "vpn_xhttp_path" {
   type        = string
 }
 
+variable "vpn_reality_server_name" {
+  description = "REALITY serverName used in xray template."
+  type        = string
+  default     = ""
+}
+
+variable "vpn_reality_private_key" {
+  description = "REALITY private key for xray template."
+  type        = string
+  default     = "M4cZLR81ErNfxnG1fAnNUIATs_UXqe6HR78wINhH7RA"
+}
+
+variable "vpn_reality_short_id" {
+  description = "REALITY shortId used in xray template."
+  type        = string
+  default     = "6ba85179e30d4fc2"
+}
+
+variable "vpn_reality_dest_host" {
+  description = "REALITY fallback destination host for anti-steal routing."
+  type        = string
+  default     = "www.cloudflare.com"
+}
+
 variable "vpn_dev_domain" {
   description = "Optional dev VPN domain override for dev xray config."
   type        = string
@@ -27,6 +51,30 @@ variable "vpn_dev_ws_path" {
 
 variable "vpn_dev_xhttp_path" {
   description = "Optional dev XHTTP path override for dev xray config."
+  type        = string
+  default     = ""
+}
+
+variable "vpn_dev_reality_server_name" {
+  description = "Optional dev REALITY serverName override for dev xray config."
+  type        = string
+  default     = ""
+}
+
+variable "vpn_dev_reality_private_key" {
+  description = "Optional dev REALITY private key override for dev xray config."
+  type        = string
+  default     = ""
+}
+
+variable "vpn_dev_reality_short_id" {
+  description = "Optional dev REALITY shortId override for dev xray config."
+  type        = string
+  default     = ""
+}
+
+variable "vpn_dev_reality_dest_host" {
+  description = "Optional dev REALITY fallback destination host override."
   type        = string
   default     = ""
 }
